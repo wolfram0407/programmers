406f5cd0170ebd2ff7be89be522f3f70637b7f86
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2023-10-23 23:23:19 +0900

    [level unrated] Title: 문자열 곱하기, Time: 1.88 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        for(int a=0;a<k;a++){
+            answer+= my_string;
+        }
+        
+        return answer;
+    }
+}
