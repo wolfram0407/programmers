6c8673c0f1e6016ecd90508b10120606f78cafcf
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2023-11-16 10:57:02 +0900

    Update 3진법 뒤집기.js

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -15,9 +15,9 @@ function solution(n) {
 /*
 function solution(n) {
     let answer = 0;
-    let test = Array.from(n.toString(3));
-    for( let a in test){
-        answer += test[a] * Math.pow(3,a);
+    let num = n.toString(3);
+    for( let a in num){
+         answer += num[a] * Math.pow(3,a);
     }
     return answer;
 }
